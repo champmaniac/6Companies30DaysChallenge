350e888eb47ba66cacf5c6bdccb642b27b2a5d67
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-02 21:07:29 +0530

    GS

---
+++ Goldman Sachs/GS_Q.14.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int i=0,j=0;
+        int mini = INT_MAX;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            while(sum>=target){
+                mini = min(mini,(i-j+1));
+                sum-=nums[j];
+                j++;
+            }
+        }
+        if(mini == INT_MAX)
+            return 0;
+        return mini;
+    }
+};
