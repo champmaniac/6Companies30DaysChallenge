dfe6e7b1bb516bb7ecd49e85461b8a90050cd1ab
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-03 20:09:22 +0530

    GS

--- Goldman Sachs/GS_Q.12.cpp
+++ Goldman Sachs/GS_Q.12.cpp
@@ -6,5 +6,10 @@ class Solution {
         if(n==1)
             return 1;
         return (n * (n + 1) / 2) * (2*n + 1) / 3;
+
+        // alternative
+        // if(n==1)
+        //     return 1;
+        // return (n*(n + 1))*(2*n + 1)/6;
     }
 };
