bb475d2a5c2376e4121f5f47a24da11aec91e9f2
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-04 21:01:43 +0530

    GS

--- Goldman Sachs/GS_Q.8.cpp
+++ Goldman Sachs/GS_Q.8.cpp
@@ -1,31 +1,20 @@
 // TC O(N) SC O(N)
 
 class Solution {
-  public:
-    int CountWays(string s){
-        // Code here
+public:
+    int numDecodings(string s) {
         int n = s.size();
-        int dp[n+1];
-        int mod = 1e9+7;
-        dp[0]=1;
-        dp[1]=1;
-        
-        if(n==1 && s[0]!='0'){
-            return 1;
+        vector<int> dp(n+1);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0')
+                dp[i]=0;
+            else{
+                dp[i]=dp[i+1];
+                if(i<n-1 && (s[i]=='1'|| s[i]=='2' && s[i+1]<'7'))
+                    dp[i]+=dp[i+2];
+            }
         }
-        if(s[0]=='0')
-        {
-            return 0;
-        }
-        
-        for(int i=2;i<=n;i++){
-            dp[i]=0;
-            if(s[i-1]>'0')
-                dp[i]=dp[i-1];
-            if(s[i-2]=='1' || s[i-2]=='2' && s[i-1]<'7')
-                dp[i]= (dp[i]+dp[i-2])%mod;
-        }
-        return dp[n];
+        return s.empty()?0:dp[0];
     }
-
 };
