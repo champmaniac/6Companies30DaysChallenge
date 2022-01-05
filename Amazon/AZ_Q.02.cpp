c3ffed606a5d2b4b17b0af378b63646dde1de73a
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-05 15:17:24 +0530

    AZ

--- Amazon/AZ_Q.02.cpp
+++ Amazon/AZ_Q.02.cpp
@@ -6,10 +6,10 @@ public:
         if(n<3) return 0;
         int res=0;
         vector<int> up(n,0), down(n,0);
-        for(int i=n-2;i>=0;--i)
+        for(int i=n-2;i>=0;--i) // backward pass
             if(arr[i]>arr[i+1])
                 down[i]=down[i+1]+1;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ // forward pass
             if(i>0 && arr[i]>arr[i-1])
                 up[i]=up[i-1]+1;
             if(up[i]&&down[i])
