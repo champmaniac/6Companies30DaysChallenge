a7930765c58d2a17f08c3f656e2c7e16704e411d
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-11 13:43:21 +0530

    Amazon

---
+++ Microsoft/MS_Q.03.cpp
@@ -0,0 +1,21 @@
+// TC O(N^N)
+void transpose(vector<vector<int> >& matrix){
+    int n = matrix.size();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+}
+void rotate(vector<vector<int> >& matrix)
+{
+    // Your code goes here
+    int n = matrix.size();
+    transpose(matrix);
+    int i=0,j=n-1;
+    while(i<j){
+        swap(matrix[i],matrix[j]);
+        i++;
+        j--;
+    }
+}
