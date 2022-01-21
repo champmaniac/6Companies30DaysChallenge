2ccb888d02336ceb025c0de7635b99385de2f42c
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-21 22:57:37 +0530

    Adobe

---
+++ Intuit/IT_Q.15.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1, r=1000000000;
+        while(l<r){
+            int mid = (l+r)/2, total =0;
+            for(auto p:piles){
+                total+=(p+mid-1)/mid;
+            }
+            if(total>h){
+                l=mid+1;
+            }
+            else
+                r=mid;
+        }
+        return l;
+    }
+};
