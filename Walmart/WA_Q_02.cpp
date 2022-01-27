f42962951f87a44c4d2e9c97c64091cf9ea224db
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-27 10:57:32 +0530

    Walmart

---
+++ Walmart/WA_Q_02.cpp
@@ -0,0 +1,11 @@
+// Either we can just return true or do the dp as shown below
+
+bool stoneGame(vector<int>& p) {
+        int n = p.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) dp[i][i] = p[i];
+        for (int d = 1; d < n; d++)
+            for (int i = 0; i < n - d; i++)
+                dp[i][i + d] = max(p[i] - dp[i + 1][i + d], p[i + d] - dp[i][i + d - 1]);
+        return dp[0][n - 1] > 0;
+    }
